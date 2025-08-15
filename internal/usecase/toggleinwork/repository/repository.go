0f9deacc31f8b0391c0b3704c24609a2bd190af9
Yuri Tihonov <tihonov-yuri@mail.ru>
2025-08-15 23:05:50 +0300

    Implement

--- internal/usecase/toggleinwork/repository/repository.go
+++ internal/usecase/toggleinwork/repository/repository.go
@@ -20,9 +20,16 @@ func New(db *sqlx.DB) *repository {
 func (r *repository) ToggleInWork(userID int64, userTaskIDs []int64, value bool) error {
 	op := "update inWork"
 
+	var taken_at sq.Sqlizer
+	if value {
+		taken_at = sq.Expr("NOW()")
+	} else {
+		taken_at = sq.Expr("NULL")
+	}
 	builder := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).
 		Update("tasks").
 		Set("is_in_work", value).
+		Set("taken_at", taken_at).
 		Where(sq.And{sq.Eq{"user_id": userID}, sq.Eq{"user_task_id": userTaskIDs}, sq.Eq{"completed": false}, sq.Eq{"is_in_work": !value}})
 
 	query, args, err := builder.ToSql()
